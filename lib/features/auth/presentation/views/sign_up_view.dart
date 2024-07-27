f97e2d84cfc2124fba65bb9fd6da18e7a5f988a0
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2024-07-27 23:43:58 +0300

    create get it service

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/services/get_it_service.dart';
 import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruits_hub/features/auth/presentation/cubits/signup_cubits/signup_cubit.dart';
 
 import 'widgets/sign_up_view_body.dart';
 
@@ -8,9 +12,12 @@ class SignUpView extends StatelessWidget {
   static const routeName = 'signup';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'حساب جديد'),
-      body: const SignUpViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'حساب جديد'),
+        body: const SignUpViewBody(),
+      ),
     );
   }
 }
