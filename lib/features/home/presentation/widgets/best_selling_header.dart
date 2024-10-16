deecb77882780751c7619a67451726c5d8f34908
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2024-10-16 04:51:58 +0300

    on click to view best selling

--- lib/features/home/presentation/widgets/best_selling_header.dart
+++ lib/features/home/presentation/widgets/best_selling_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
+import 'package:fruits_hub/features/best_selling_fruits/presentation/best_selling_view.dart';
 
 class BestSellingHeader extends StatelessWidget {
   const BestSellingHeader({super.key});
@@ -13,10 +14,13 @@ class BestSellingHeader extends StatelessWidget {
           'الأكثر مبيعًا',
           style: TextStyles.bold16,
         ),
-        Text(
-          'المزيد',
-          style: TextStyles.regular13.copyWith(
-            color: const Color(0xff949D9E),
+        GestureDetector(
+          onTap: () => Navigator.pushNamed(context, BestSellingView.routeName),
+          child: Text(
+            'المزيد',
+            style: TextStyles.regular13.copyWith(
+              color: const Color(0xff949D9E),
+            ),
           ),
         ),
       ],
