51138234328186ec1243d7f198c207c21fdb3a76
mohhamedsalahh <tktkyttttttttttk@gmail.com>
2025-01-06 17:15:19 +0200

    create best selling view

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 
 AppBar buildAppBar(BuildContext context,
-    {required String title, void Function()? onTap}) {
+    {required String title, void Function()? onTap, List<Widget>? actions}) {
   return AppBar(
+    actions: actions,
     centerTitle: true,
     backgroundColor: Colors.white,
+    elevation: 0,
     title: Text(
       title,
       style: TextStyles.bold19,
--- lib/features/home/presentation/widgets/custom_home_appbar.dart
+++ lib/features/home/presentation/widgets/custom_home_appbar.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_images.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
-import 'package:svg_flutter/svg.dart';
+
+import 'notifications_widget.dart';
 
 class CustomHomeAppBar extends StatelessWidget {
   const CustomHomeAppBar({super.key});
@@ -21,14 +22,7 @@ class CustomHomeAppBar extends StatelessWidget {
         'أحمد مصطفي',
         style: TextStyles.bold16,
       ),
-      trailing: Container(
-        padding: const EdgeInsets.all(12),
-        decoration: const ShapeDecoration(
-          shape: OvalBorder(),
-          color: Color(0xffEEF8ED),
-        ),
-        child: SvgPicture.asset(Assets.imagesNotification),
-      ),
+      trailing: const NotificationWidget(),
     );
   }
 }
